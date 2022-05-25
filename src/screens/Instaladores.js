6f5f27030a22ccf9c74143e58cb9f03f94d75bb3
G-Ilian <gbiel.boza@gmail.com>
2022-05-25 14:05:28 -0300

    Criada tela para os instaladores

---
+++ src/screens/Instaladores.js
@@ -0,0 +1,10 @@
+// Imports necessários para rodar a aplicação
+import React from 'react'
+import {Text} from 'react-native'
+
+
+// Função para criar a tela de instaladores
+// Apenas um teste
+export default function Instaladores(){
+    return <Text>Instaladores</Text>
+}
