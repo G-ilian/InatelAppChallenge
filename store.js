44d3d4ee80e7a67353a4db5e9a612c031bafa091
G-Ilian <gbiel.boza@gmail.com>
2022-05-26 02:28:06 -0300

    Serve para documentar todas as operações feitas com o redux JS

---
+++ store.js
@@ -0,0 +1,9 @@
+// Serve para documentar todas as operações feitas com o redux JS
+import {configureStore} from "@reduxjs/toolkit";
+import navReducer from "./slices/navSlice";
+
+export const store = configureStore({
+    reducer:{
+        nav :navReducer
+    }
+});
