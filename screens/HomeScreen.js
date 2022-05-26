7db06b2311168cbdae5285d3990b595b5b5869ca
G-Ilian <gbiel.boza@gmail.com>
2022-05-26 18:13:10 -0300

    Tela de Home da aplicação, conta com elementos visuais e de navegação

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,24 @@
 import React from "react";
-import{StyleSheet,Text,SafeAreaView} from "react-native";
+import{StyleSheet,Text,SafeAreaView,Image,View} from "react-native";
+import tw from 'tailwind-react-native-classnames';
+import NavOptions from "../components/NavOptions";
+
+
 
 const HomeScreen=()=>{
     return(
-        <SafeAreaView>
-            <Text>Teste</Text>
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image 
+                    style={{
+                        width:200,height:200,resizeMode:'contain'
+                    }}
+                    source={{
+                        uri:'https://upload.wikimedia.org/wikipedia/commons/9/96/VIASAT_official.jpg',
+                    }}/>
+            <NavOptions/>
+            </View>
+        
         </SafeAreaView>
         )
 
@@ -12,4 +26,6 @@ const HomeScreen=()=>{
 
 export default HomeScreen;
 
-const styles=StyleSheet.create({});
+const styles=StyleSheet.create({
+
+});
