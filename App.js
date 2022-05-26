ba350995cff4c12a6837138fd0ed65daf6c3afa8
G-Ilian <gbiel.boza@gmail.com>
2022-05-26 18:12:30 -0300

    Estrutura modificada para abrir a partir da tela de HomeScreen

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
 import {Provider} from "react-redux";
-import {store} from './store'
-import HomeScreen from './screens/HomeScreen'
+import {store} from './store';
+import HomeScreen from './screens/HomeScreen';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>  
     </Provider>
 
   );
