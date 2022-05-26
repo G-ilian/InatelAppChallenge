4947a2bd2789ddd9c0987c88334e0ae2cf340702
G-Ilian <gbiel.boza@gmail.com>
2022-05-26 02:27:38 -0300

    Versão base da HomeScreen, ainda não funciona

---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import{StyleSheet,Text,SafeAreaView} from "react-native";
+
+const HomeScreen=()=>{
+    return(
+        <SafeAreaView>
+            <Text>Teste</Text>
+        </SafeAreaView>
+        )
+
+}
+
+export default HomeScreen;
+
+const styles=StyleSheet.create({});
