d26fafbbcd4e8b2d3b848da91d2d4436a4d967ac
G-Ilian <gbiel.boza@gmail.com>
2022-05-27 01:01:09 -0300

    Adicionada imagens a página inicial, que ao serem tocadas permitem transições entre telas

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -3,19 +3,21 @@ import{StyleSheet,Text,SafeAreaView,Image,View} from "react-native";
 import {FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import tw from 'tailwind-react-native-classnames';
 import {Icon} from 'react-native-elements';
+import { useNavigation } from "@react-navigation/native";
 
 // Lista de dados com imagens e textos para as opções
 const data=[
     {
         id:"1",
         title:"Contrate nossos planos",
-        image:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/FP_Satellite_icon.svg/1200px-FP_Satellite_icon.svg.png',
-        screen:'MapScreen'
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/FP_Satellite_icon.svg/1200px-FP_Satellite_icon.svg.png",
+        screen:"MapScreen"
     }
 ]
 
 // Tela inicial da aplicação e suas navegações
 const NavOptions=()=>{
+    const navigation=useNavigation();
     return(
         <FlatList
         
@@ -23,8 +25,10 @@ const NavOptions=()=>{
             keyExtractor={(item)=>item.id}
             horizontal
             renderItem={({item})=>(
-                <TouchableOpacity style={tw `p-2 pl-6 pb-4 bg-gray-200 m-2 w-40`}>
-
+                <TouchableOpacity 
+                onPress={()=>navigation.navigate(item.screen)}
+                style={tw `p-2 pl-6 pb-4 bg-gray-200 m-2 w-40`}>
+                    
                     <View>
                         <Image
                             style={{width:100,height:100,resizeMode:"contain"}}
@@ -33,7 +37,7 @@ const NavOptions=()=>{
                     </View>
                     <Text style={tw `mt-2 text-lg font-semibold`}>{item.title}</Text>
                     <Icon 
-                    style={tw `p-2 bg-white rounded-full - w-10 mt-4`}
+                    style={tw `p-2 bg-white rounded-full  w-10 mt-4`}
                     name="arrowright" color="black" type="antdesign"/>
                 </TouchableOpacity>
             )}
