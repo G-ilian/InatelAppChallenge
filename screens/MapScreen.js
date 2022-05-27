bb0e75a48d0f3c75cecf09f6c24ddfdb3d4dd9cc
G-Ilian <gbiel.boza@gmail.com>
2022-05-27 01:02:32 -0300

    Ainda sem alterações, apenas teste

---
+++ screens/MapScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import{StyleSheet,Text,SafeAreaView,Image,View} from "react-native";
+
+// Tela inicial da aplicação e suas navegações
+const MapScreen=()=>{
+    return(
+        <View>
+            <Text>Stuff....</Text>
+        </View>
+    )
+
+}
+
+export default MapScreen;
