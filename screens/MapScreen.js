246dd3e292ce27bd1bed95a6e6ca51b11ef78ffa
G-Ilian <gbiel.boza@gmail.com>
2022-05-28 02:38:23 -0300

    Adicionada stack de navegação, que permite mudar de telas, da tela de procura, para tela de planos e instalações agendadas

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,48 @@
 import React from "react";
-import{StyleSheet,Text,SafeAreaView,Image,View} from "react-native";
+import{StyleSheet,View,Text} from "react-native";
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_APIKEY} from "@env";
+import MapView from 'react-native-maps';
+import tw from 'tailwind-react-native-classnames';
+import Map from "../components/Map";
+import UserOptions from "../components/UsersOptions";
+import { createStackNavigator } from "@react-navigation/stack";
+import NavigateCard from "../components/NavigateCard";
 
 // Tela inicial da aplicação e suas navegações
 const MapScreen=()=>{
+    const Stack=createStackNavigator();
     return(
         <View>
-            <Text>Stuff....</Text>
-        </View>
-    )
+            <View style={tw `h-1/2`}>
+                <Map/>
+            </View>
+            
+            <View style={tw `h-1/2`}>
 
-}
+            <Stack.Navigator>
+
+                <Stack.Screen
+                    name="Navigate Card"
+                    component={NavigateCard}
+                    options={{
+                        headerShown:false,
+                    }}
+                />
+                
+                <Stack.Screen
+                    name="User Options"
+                    component={UserOptions}
+                    options={{
+                        headerShown:false,
+                    }}
+                />
+            </Stack.Navigator>
+            </View>
+        </View>
+    );
+};
 
 export default MapScreen;
+
+const styles=StyleSheet.create({});
