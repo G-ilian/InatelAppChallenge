ed09cd926d70791825637da0899e48b07147ba96
G-Ilian <gbiel.boza@gmail.com>
2022-05-29 20:54:48 -0300

    Rota para conexão a tela de instaladores

---
+++ screens/InstaladoresScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import{StyleSheet,SafeAreaView,View,Image,Text,Icon} from "react-native";
+import InstaladoresOptions from "../components/InstaladoresOptions";
+
+const InstaladoresScreen=()=>{
+
+
+    return(
+        <SafeAreaView>
+            <InstaladoresOptions></InstaladoresOptions>
+        </SafeAreaView>
+        
+    )
+}
+
+export default InstaladoresScreen;
+
+const styles=StyleSheet.create({
+
+});
