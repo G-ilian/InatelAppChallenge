b9a39b627e40f5fe9362d7f879900c02cf3834f6
G-Ilian <gbiel.boza@gmail.com>
2022-05-29 21:02:03 -0300

    Adicionada box para fazer buscas já na tela principal

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,13 +4,16 @@ import tw from 'tailwind-react-native-classnames';
 import NavOptions from "../components/NavOptions";
 import { useDispatch } from "react-redux";
 import { setDestination,setOrigin } from "../slices/navSlice";
-
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import {GOOGLE_MAPS_APIKEY} from "@env";
 
 const HomeScreen=()=>{
     const dispatch=useDispatch();
 
     return(
         <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw `border-t border-gray-200 flex-shrink`}>
+        </View>
             <View style={tw`p-5`}>
                 <Image 
                     style={{
@@ -20,7 +23,32 @@ const HomeScreen=()=>{
                         uri:'https://upload.wikimedia.org/wikipedia/commons/9/96/VIASAT_official.jpg',
                     }}
                 />
-            <NavOptions/>
+                <View>
+                    <GooglePlacesAutocomplete
+                        placeholder="Onde você está ?"
+                        styles={toInputBoxStyles}
+                        nearbyPlacesAPI="GooglePlacesSearch"
+                        enablePoweredByContainer={false}
+                        debounce={400}
+                        returnKeyType={"search"}
+                        minLength={2}
+                        fetchDetails={true}
+                        onPress={(data,details=null)=>{
+                            dispatch(setOrigin({
+                                location:details.geometry.location,
+                                description:data.description,
+                            }));
+                            dispatch(setDestination(null));
+                            
+                        }}
+                        query={{
+                            key:GOOGLE_MAPS_APIKEY,
+                            language:"pt-br"
+                        }}
+
+                    />
+                </View>
+                <NavOptions/>
             </View>
         
         </SafeAreaView>
@@ -30,6 +58,19 @@ const HomeScreen=()=>{
 
 export default HomeScreen;
 
-const styles=StyleSheet.create({
-
+const toInputBoxStyles = StyleSheet.create({
+    container:{
+        backgroundColor:"white",
+        paddingTop:2,
+        flex:0
+    },
+    textInput:{
+        backgroundColor:"#DDDDDF",
+        borderRadius:7,
+        fontSize:18
+    },
+    textInputContainer:{
+        paddingHorizontal:5,
+        paddingBottom:2
+    }
 });
